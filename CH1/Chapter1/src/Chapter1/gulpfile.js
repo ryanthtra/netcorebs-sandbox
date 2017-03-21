7b7baf100c0aced996a3e1032d6b51e76a4d0a9b
Ryan Transfiguracion <rtransfig@gmail.com>
2017-03-21 03:20:15 -0700

    Added Bootstrap via bower.  Added sass and gulp via npm.  Added HomeController and Index.cshtml.

---
+++ CH1/Chapter1/src/Chapter1/gulpfile.js
@@ -0,0 +1,14 @@
+﻿/*
+This file is the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+var gulpSass = require('gulp-sass');
+
+gulp.task('compile-sass', function() 
+{
+  gulp.src('./wwwroot/lib/bootstrap/scss/bootstrap.scss')
+    .pipe(gulpSass())
+    .pipe(gulp.dest('./wwwroot/css'));
+});
